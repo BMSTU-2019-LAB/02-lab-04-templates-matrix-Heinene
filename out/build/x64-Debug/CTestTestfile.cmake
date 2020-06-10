4fe6da8fbf64d17ea88ce4da33e4cefa20fa4575
Holwarn1 aa <herteret@gmail.com>
2020-06-10 14:42:15 +0300

    dd

---
+++ out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/User/source/repos/BMSTU-2019-LAB/02-lab-04-templates-matrix-Heinene
+# Build directory: C:/Users/User/source/repos/BMSTU-2019-LAB/02-lab-04-templates-matrix-Heinene/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unit_tests "C:/Users/User/source/repos/BMSTU-2019-LAB/02-lab-04-templates-matrix-Heinene/out/build/x64-Debug/tests.exe")
+set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/User/source/repos/BMSTU-2019-LAB/02-lab-04-templates-matrix-Heinene/CMakeLists.txt;55;add_test;C:/Users/User/source/repos/BMSTU-2019-LAB/02-lab-04-templates-matrix-Heinene/CMakeLists.txt;0;")
